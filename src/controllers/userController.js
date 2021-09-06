4774b20428db302400d817dc5323333e6396ba1f
rlawnsgh714 <80739316+rlawnsgh714@users.noreply.github.com>
2021-09-06 18:05:56 +0900

    json files

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,11 @@
 import User from "../models/User";
-import fetch from "node-fetch";
 import bcrypt from "bcrypt";
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-export const getJoin = (req, res) =>
-  res.render("users/join", { pageTitle: "Join" });
+let joinedUser = null;
 
 export const postJoin = async (req, res) => {
   const {
-    username,
     password,
     password2,
     grade,
@@ -18,129 +15,156 @@ export const postJoin = async (req, res) => {
     name,
     isAgree,
   } = req.body;
-  if (!isAgree) {
-    return res.status(400).render("users/join", {
-      pageTitle,
-      errorMessage: "개인정보 수집에 동의해주세요.^^",
+  console.log(req.body);
+  if (isAgree !== true) {
+    return res.status(400).json({
+      error: "Please agree to the collection of personal information.",
+      //개인정보 수집에 동의해주세요.
     });
-  }
-  if(password){
-    return res.render("src/nodemailer/join",)
   }
   const pageTitle = "Join";
   if (password !== password2) {
-    return res.status(400).render("users/join", {
-      pageTitle,
-      errorMessage: "비밀번호가 일치하지 않습니다.",
+    return res.status(400).json({
+      error: "Password is different",
+      //비밀번호가 일치하지 않습니다
     });
   }
-  const exists = await User.exists({ $or: [{ username }, { email }] });
+  const exists = await User.exists({ email: email });
   if (exists) {
-    return res.status(400).render("users/join", {
-      pageTitle,
-      errorMessage:
-        "이 username/email은 이미 사용되고 있습니다. 다른 username/email로 바꿔주세요.",
+    return res.status(400).json({
+      error: "This email is already in use, Please change to another email.",
+      //이 email은 이미 사용되고 있습니다. 다른 email로 바꿔주세요.
     });
   }
   try {
-    await User.create({
+    joinedUser = await User.create({
       email,
-      username,
       password,
       name,
       grade,
       classNumber,
       stuNumber,
     });
-    return res.redirect("/login");
+    return res.status(200).json({
+      message: "succeeded register!",
+      //회원가입 성공
+    });
   } catch (error) {
-    return res.status(400).render("users/join", {
-      pageTitle,
-      errorMessage: error._message,
+    return res.status(400).json({
+      error: "Registration failed, Please try again.",
+      //회원가입에 실패하였습니다. 다시 시도해주십시오.
     });
   }
 };
 
 export const getEmailAuthorization = (req, res) => {
-  return res.render("users/email-auth", { pageTitle: "Email Authorization" });
+  if (joinedUser === null || joinedUser.isValid === true) {
+    res.status(400).json({
+      error: "It is an already authenticated account or abnormal access.",
+      //이미 인증된 계정이거나 비정상적인 접근입니다.
+    });
+  }
+  const sendName = "glassfromb1nd@gmail.com";
+  const password = process.env.EMAIL_PASSWORD;
+  const recName = joinedUser.email;
+  const confirmationCode = Math.floor(Math.random() * 8999) + 1000;
+  joinedUser.confirmationCode = confirmationCode;
+
+  const transporter = nodemailer.createTransport({
+    secure: false,
+    service: "Gmail",
+    auth: {
+      user: sendName,
+      pass: password,
+    },
+  });
+
+  const mailOptions = {
+    from: "glassfromb1nd@gmail.com",
+    to: recName,
+    subject: "GLASS 회원가입 인증번호",
+    text: `안녕하세요!
+    회원가입을 위해 확인 코드를 웹 페이지에 입력해주세요.
+      
+    확인 코드: ${confirmationCode}
+
+    GLASS 서버 팀`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.status(400).json({
+        error: "Failed to send mail.",
+        //메일 발송에 실패하였습니다.
+      });
+    }
+    console.log("Message %s sent: %s", info.messageId, info.response);
+  });
+  return res.status(200).json({
+    message: "Succeeded to send mail.",
+    //
+  });
 };
 
-export const getLogin = (req, res) => {
-  return res.render("users/login", { pageTitle: "Login" });
+export const postEmailAuthorization = async (req, res) => {
+  const {
+    body: { confirmation },
+  } = req;
+
+  if (Number(joinedUser.confirmationCode) !== Number(confirmation)) {
+    await User.findByIdAndDelete(joinedUser._id);
+    joinedUser = null;
+    return res.status(400).json({
+      error: "The email verification number is incorrect. Please re-enter.",
+      //이메일 인증 번호가 옳지 않습니다. 다시 입력해주세요.
+    });
+  }
+
+  joinedUser.isValid = true;
+  joinedUser.save();
+  return res.status(200).json({
+    message: "Email Verification Success!",
+    //Email인증 성공!
+  });
 };
 
 export const postLogin = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ email });
+  if (!user.isValid)
+    return res.status(400).json({
+      error: "Email is not verified.",
+      //Email 인증이 되지 않았습니다.
+    });
   if (!user) {
-    return res.status(400).render("users/login", {
-      pageTitle,
-      errorMessage: "이 username을 가진 계정이 존재하지 않습니다.",
+    return res.status(400).json({
+      error: "An account with this email does not exist.",
+      //이 Email을 가진 계정이 존재하지 않습니다.
     });
   }
   const ok = await bcrypt.compare(password, user.password);
   if (!ok) {
-    return res.status(400).render("users/login", {
-      pageTitle,
-      errorMessage: "비밀번호가 옳지 않습니다.",
+    return res.status(400).json({
+      error: "The password is incorrect.",
+      //비밀번호가 옳지 않습니다.
     });
   }
   req.session.loggedIn = true;
   req.session.user = user;
-  return res.redirect("/");
+  return res.status(200).json({
+    message: "Succeed log-in!",
+    //로그인 성공!
+  });
 };
-
-export const getEmailAuthorization = (req, res) => {
-  const send_name = "junho07140714"
-  const password = "qgfmrljkgqxjjrnh"
-  const rec_name = req.session.user.email
-
-  const cert_number = Math.floor(Math.random() * 8999) + 1000
-
-  let transporter = nodemailer.createTransport({
-    //host: 'smtp.mailtrap.io',
-    //port: 2525,
-    //secure: false, // secure:true for port 465, secure:false for port 587
-    service: 'Gmail',
-    auth: {
-        user: send_name,
-        pass: password
-    }
-  });
-
-  const mailOptions = {
-      from: 'opso@gmail.com', // sender address
-      to: rec_name, // list of receivers`
-      subject: 'OPSO 인증번호', // Subject line
-      text: `안녕하세요.
-  귀하의 장치를 인식하지 못했기 때문에 추가 확인이 필요한 서명입니다. 로그인을 완료하려면 인식할 수 없는 장치에서 확인 코드를 입력합니다.
-      
-  확인 코드: ${cert_number}
-
-  계정에 로그인하려고 시도하지 않으면 암호가 손상될 수 있습니다. https://OPSO.com/settings/security을 방문하여 OPSO 계정에 사용할 강력한 새 암호를 만드십시오.
-  감사합니다.
-
-  OPSO 서버 팀`, // plain text body
-      //html: '<b>Hello world ?</b>' // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent: %s', info.messageId, info.response);
-  });
-}
 
 export const logout = (req, res) => {
   req.session.destroy();
-  return res.redirect("/");
-};
-
-export const getEdit = (req, res) => {
-  return res.render("users/edit-profile", { pageTitle: "Edit Profile" });
+  return res.status(200).json({
+    message: "Succeed log-out!",
+    //로그아웃 성공!
+  });
 };
 
 export const postEdit = async (req, res) => {
@@ -148,24 +172,15 @@ export const postEdit = async (req, res) => {
     session: {
       user: { _id },
     },
-    body: { name, username, email },
+    body: { name, email },
     file,
   } = req;
-  const findUsername = await User.findOne({ username });
-  if (findUsername !== null) {
-    if (findUsername._id.toString() !== _id) {
-      return res.status(400).render("users/edit-profile", {
-        pageTitle: "Edit Profile",
-        errorMessage: "이 username은 이미 존재합니다.",
-      });
-    }
-  }
   const findEmail = await User.findOne({ email });
   if (findEmail !== null) {
     if (findEmail._id.toString() !== _id) {
-      return res.status(400).render("users/edit-profile", {
-        pageTitle: "Edit Profile",
-        errorMessage: "이 email은 이미 존재합니다.",
+      return res.status(400).json({
+        error: "This email already exists.",
+        //이 email은 이미 존재합니다.
       });
     }
   }
@@ -174,16 +189,14 @@ export const postEdit = async (req, res) => {
     {
       name,
       email,
-      username,
     },
     { new: true }
   );
   req.session.user = updatedUser;
-  return res.redirect("/user/edit");
-};
-
-export const getChangePassword = (req, res) => {
-  return res.render("users/change-password", { pageTitle: "Change Password" });
+  return res.status(200).json({
+    message: "Member information modification successful!",
+    //회원정보 수정 성공
+  });
 };
 
 export const postChangePassword = async (req, res) => {
@@ -196,20 +209,35 @@ export const postChangePassword = async (req, res) => {
   const user = await User.findById(_id);
   const ok = await bcrypt.compare(oldPassword, user.password);
   if (!ok) {
-    return res.status(400).render("users/change-password", {
-      pageTitle: "Change Password",
-      errorMessage: "현재 비밀번호가 옳지 않습니다.",
+    return res.status(400).json({
+      error: "The current password is incorrect.",
+      //현재 비밀번호가 옳지 않습니다.
     });
   }
   if (newPassword !== newPasswordConfirmation) {
-    return res.status(400).render("users/change-password", {
-      pageTitle: "Change Password",
-      errorMessage: "새로운 비밀번호가 일치하지 않습니다.",
+    return res.status(400).json({
+      error: "The new passwords do not match.",
+      //새로운 비밀번호가 일치하지 않습니다.
     });
   }
   user.password = newPassword;
   await user.save();
-  return res.redirect("/");
+  return res.status(200).json({
+    message: "Succeed changed password",
+    //비밀번호 변경 성공
+  });
 };
 
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id).populate("writings");
+  if (!user) {
+    return res.status(404).json({
+      error: "User not found",
+      //유저를 찾지 못했습니다.
+    });
+  }
+  return res.status(200).json({
+    user,
+  });
+};
