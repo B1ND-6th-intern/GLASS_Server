bb6fb52639783e416998ca367cfa5d9b3402e5a7
whitebear05 <woojs0505@naver.com>
2021-09-07 19:14:18 +0900

    create Comment schema

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
-  owner: {},
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
