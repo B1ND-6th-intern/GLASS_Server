a3fad9c6d33dc9fe3c8a66d49e127af98d727cfc
whitebear05 <woojs0505@naver.com>
2021-09-15 20:57:38 +0900

    Update commentRouter

--- src/routers/commentRouter.js
+++ src/routers/commentRouter.js
@@ -14,3 +14,5 @@ commentRouter.post("/:id([0-9a-f]{24})/upload", postUploadComment);
 commentRouter.get("/:id([0-9a-f]{24})/edit", getEditComment);
 commentRouter.post("/:id([0-9a-f]{24})/edit", postEditComment);
 commentRouter.delete("/:id([0-9a-f]{24})", deleteComment);
+
+export default commentRouter;
