8bb78ec1ba7f618452776fb7ad054b3dfd3fa319
whitebear05 <woojs0505@naver.com>
2021-10-06 23:24:03 +0900

    Fix: token verify

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,17 +2,15 @@ import jwt from "jsonwebtoken";
 import multer from "multer";
 
 export const authenticateAccessToken = (req, res, next) => {
-  console.log("I'm in authentication");
-  let authHeader = req.headers["authorization"];
-  let token = authHeader && authHeader.split(" ");
-  console.log(token);
-  if (!token[1]) {
+  let authHeader = req.headers.authorization;
+  let token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
     return res.status(400).json({
       status: 400,
       error: "토큰 포맷이 잘못 되었거나 토큰이 보내지지 않았습니다.",
     });
   }
-  jwt.verify(token[1], process.env.JWT_SECRET, (error, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
     if (error) {
       console.log("토큰 에러 : " + error);
       return res.status(403).json({
@@ -20,7 +18,6 @@ export const authenticateAccessToken = (req, res, next) => {
         error: "토큰 인증 과정에서 오류가 발생했습니다.",
       });
     }
-    console.log(user);
     req.user = user;
     next();
   });
