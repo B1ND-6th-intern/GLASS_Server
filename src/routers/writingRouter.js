51198be7d42b0a9b9fc2cab3df5a884521ed8354
whitebear05 <woojs0505@naver.com>
2021-10-20 20:37:47 +0900

    Update

--- src/routers/writingRouter.js
+++ src/routers/writingRouter.js
@@ -23,11 +23,7 @@ writingRouter.delete(
   authenticateAccessToken,
   deleteWriting
 );
-<<<<<<< HEAD
-writingRouter.post("/like/:id([0-9a-f]{24})", registerWritingLike);
-=======
 writingRouter.get("/like/:id([0-9a-f]{24})", registerWritingLike);
->>>>>>> 3d92541c906c0623d33a43c7133b10259780f49c
 writingRouter.post("/upload", authenticateAccessToken, postUpload);
 writingRouter.post("/upload/imgs", imgsUpload.array("img", 10), postUploadImgs);
 
