5e84e070333407ec0c622d4c203734459f302e4e
whitebear05 <woojs0505@naver.com>
2021-10-21 20:24:09 +0900

    Add: infinite scroll controller

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { postJoin, postLogin, search } from "../controllers/userController";
-import { getPosts } from "../controllers/writingController";
+import {
+  getInfiniteScrollPosts,
+  getPosts,
+} from "../controllers/writingController";
 // import { postquestion } from "../controllers/questionController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/posts", getPosts);
+rootRouter.get("/posts/:index", getInfiniteScrollPosts);
 rootRouter.post("/join", postJoin);
 rootRouter.post("/login", postLogin);
 rootRouter.get("/search", search);
